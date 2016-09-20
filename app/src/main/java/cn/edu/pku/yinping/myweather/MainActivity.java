661f7bed715cb1c074d7f3f63ba316763aa4a566
yinping1103 <1020856468@qq.com>
2016-09-20 21:04:49 +0800

    first commit

---
+++ app/src/main/java/cn/edu/pku/yinping/myweather/MainActivity.java
@@ -0,0 +1,16 @@
+package cn.edu.pku.yinping.myweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ag on 9/20/16.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wether_info);
+    }
+}
